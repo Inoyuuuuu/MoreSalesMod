458a2c84a994e5484b8920bf13b212e61333c89e
Inoyuuuuu <inoyuuuuu@gmail.com>
2025-12-30 23:24:46 +0100

    lethalLibFix

--- MoreSales/Patches/IncreaseSalesPatch.cs
+++ MoreSales/Patches/IncreaseSalesPatch.cs
@@ -1,7 +1,9 @@
 using GameNetcodeStuff;
 using HarmonyLib;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace MoreSales.Patches
@@ -25,7 +27,8 @@ namespace MoreSales.Patches
             int maxAmountOfItemsOnSale = MoreSales.moreSalesConfigs.maxNumberOfItemsOnSale;
             float amountOfSalesModifier = MoreSales.moreSalesConfigs.moreSalesOdds / 50f;
             minAmountOfItemsOnSale = (minAmountOfItemsOnSale > maxAmountOfItemsOnSale) ? maxAmountOfItemsOnSale : minAmountOfItemsOnSale;
-
+            
+            RemoveNotFullyRemovedItems(__instance);
             __instance.InitializeItemSalesPercentages();
 
             List<int> list = [];
@@ -53,10 +56,11 @@ namespace MoreSales.Patches
                 amountOfItemsOnSale = list.Count;
             }
 
-            //MoreSales.mls.LogWarning("amountofitemsonsale: " + amountOfItemsOnSale);
+            MoreSales.mls.LogWarning("amountofitemsonsale: " + amountOfItemsOnSale);
 
             for (int k = 0; k < amountOfItemsOnSale; k++)
             {
+
                 if (list.Count <= 0)
                 {
                     break;
@@ -66,12 +70,13 @@ namespace MoreSales.Patches
 
                 float randomSalesStrength = Math.Clamp(((float)random.Next(0, 100) / 100f) * saleValueModifier, 0, 1);
                 int randomSalesPercentage = (int)Math.Round((double)Mathf.Lerp(minSalesPercentage, maxSalesPercentage, randomSalesStrength));
-                //MoreSales.mls.LogWarning("randomSalesPercentage: " + randomSalesPercentage + " --> " + RoundToNearestTen(randomSalesPercentage));
+                MoreSales.mls.LogWarning("randomSalesPercentage: " + randomSalesPercentage + " --> " + RoundToNearestTen(randomSalesPercentage));
 
                 if (MoreSales.moreSalesConfigs.roundToNearestTen)
                 {
                     randomSalesPercentage = RoundToNearestTen(randomSalesPercentage);
                 }
+
                 __instance.itemSalesPercentages[num3] = (100 - randomSalesPercentage);
 
                 list.Remove(num3);
@@ -83,12 +88,81 @@ namespace MoreSales.Patches
             return (int)Math.Round((double)value / 10.0) * 10;
         }
 
-        //[HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.PlayerJump))]
-        //[HarmonyPostfix]
-        //public static void DebugPatch()
-        //{
-        //    Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
-        //    terminal.SetItemSales();
-        //}
+        private static bool WasItemRemoved(Item item)
+        {
+            Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+            return terminal.ParseWord(item.itemName.ToLower()) == null;
+        }
+
+        private static void RemoveNotFullyRemovedItems(Terminal terminal)
+        {
+            List<Item> newItems = [];
+
+            for (int i = 0; i < terminal.buyableItemsList.Length; i++)
+            {
+                if (terminal.ParseWord(terminal.buyableItemsList[i].itemName.ToLower()) != null)
+                {
+                    newItems.Add(terminal.buyableItemsList[i]);
+                } else
+                {
+                    MoreSales.mls.LogWarning("tn c " + terminal.terminalNodes.terminalNodes.Count);
+                    MoreSales.mls.LogWarning("stn c " + terminal.terminalNodes.specialNodes.Count);
+
+                    for (int k = 0; k < terminal.terminalNodes.specialNodes.Count; k++)
+                    {
+                        MoreSales.mls.LogWarning("node: " + terminal.terminalNodes.specialNodes[k].name.ToLower() 
+                            + " --------- item: " + terminal.buyableItemsList[i].itemName.ToLower());
+
+                        if (terminal.terminalNodes.specialNodes[k].name.ToLower().Equals(terminal.buyableItemsList[i]))
+                        {
+                            terminal.terminalNodes.specialNodes.RemoveAt(k);
+                        }
+                    }
+                }
+            }
+
+            terminal.buyableItemsList = newItems.ToArray();
+        }
+
+        [HarmonyPatch(typeof(StartOfRound), nameof(StartOfRound.Start))]
+        [HarmonyPostfix]
+        public static void RedoSalesPatch(StartOfRound __instance)
+        {
+            __instance.StartCoroutine(DelayedSetItemSales(4));
+        }
+
+        public static IEnumerator DelayedSetItemSales(int seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+
+            Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+            terminal.SetItemSales();
+
+            MoreSales.mls.LogWarning("itemlist: " + terminal.buyableItemsList.Length + " itemSalesPercentages: " + terminal.itemSalesPercentages.Length);
+            for (int i = 0; i < terminal.buyableItemsList.Length; i++)
+            {
+                MoreSales.mls.LogWarning("item: " + terminal.buyableItemsList[i].itemName + " price: " + terminal.buyableItemsList[i].creditsWorth + " sale: " + (100 - terminal.itemSalesPercentages[i]));
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerControllerB), nameof(PlayerControllerB.PlayerJump))]
+        [HarmonyPostfix]
+        public static void DebugPatch()
+        {
+            Terminal terminal = UnityEngine.Object.FindObjectOfType<Terminal>();
+            terminal.SetItemSales();
+
+            MoreSales.mls.LogWarning("itemlist: " + terminal.buyableItemsList.Length + " itemSalesPercentages: " + terminal.itemSalesPercentages.Length);
+
+            for (int i = 0; i < terminal.buyableItemsList.Length; i++)
+            {
+                //if (terminal.ParseWord(terminal.buyableItemsList[i].itemName.ToLower()) == null)
+                //{
+                //    MoreSales.mls.LogWarning("found unbuyable item: " + terminal.buyableItemsList[i].itemName);
+                //}
+
+                MoreSales.mls.LogWarning("item: " + terminal.buyableItemsList[i].itemName + " price: " + terminal.buyableItemsList[i].creditsWorth + " sale: " + (100 - terminal.itemSalesPercentages[i]));
+            }
+        }
     }
 }
